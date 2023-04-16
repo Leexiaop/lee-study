e787cdde9ce3195f37e526200658103e641dafeb
lixiaopeng06 <lixiaopeng06.baidu.com>
2023-04-17 07:56:06 +0800

    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,53 @@
 // index.js
 // const app = getApp()
-const request = require('../../assets/api/request')
-const url = require('../../assets/api/url')
-const color = require('../../utils/util')
-
+const moment = require('moment');
 Page({
 	data: {
-        asideList: [],
+        list: [
+            {
+                title: '面试和学习永无止境',
+                descList: ['前端面试', '三维开发', '服务端与项目部署', 'HTTP和安全', '地图学习', '...'],
+                background: 'yellow'
+            },
+            {
+                title: '公司真题一网打尽',
+                descList: ['字节跳动', '阿里巴巴', '腾讯', '华为', '美团', '...'],
+                background: 'hotpink'
+            },
+            {
+                title: '三维案例因有尽有',
+                descList: ['ThreeJs', 'CesuimJS', 'BabylonJS', 'WebGPU', 'WebGL', '...'],
+                background: 'chartreuse'
+            },
+            {
+                title: '资源下载',
+                descList: ['React', 'Vue2.x', 'Vue3.x', 'GLSL', 'WebGL', '...'],
+                background: 'white'
+            },
+            {
+                title: '机构面试题汇总',
+                descList: ['前锋', '黑马', '爪哇', '...'],
+                background: 'mediumturquoise'
+            }
+        ],
         isIn: true,
-        navHeight: 0
+        isFixed: false,
+        timer: ''
 	},
 	onLoad () {
-		request(url.getMenuList).then(res => {
-			this.setData({
-                asideList: res.map(item => {
-                    return {
-                        ...item,
-                        color: color.color16()
-                    }
-                })
-            });
-        })
         wx.showShareMenu({
             withShareTicket: true,
             menus: ['shareAppMessage', 'shareTimeline']
         })
     },          
 	onShow () {
-        this.setData({isIn: true})
+        this.setData({
+            isIn: true,
+            isFixed: false
+        })
+    },
+    onReachBottom (e) {
+        this.setData({isFixed: true})
     },
     onShareAppMessage() {
         return {
@@ -36,7 +56,7 @@ Page({
             imageUrl: '/assets/images/logo.png'
         }
     },
-	onGridClick (grid) {
+	onGridClick () {
         this.setData({
             isIn: false
         })
